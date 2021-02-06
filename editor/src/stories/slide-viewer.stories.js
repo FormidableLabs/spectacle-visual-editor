305d6b3a921ea51c07fa970ba6dd667469f3ff5d
Carlos Kelly <me@carlos.dev>
2021-02-06 08:20:47 -0600

    Added Storybook

---
+++ editor/src/stories/slide-viewer.stories.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Heading } from 'spectacle';
+import { SlideViewer } from '../components/slide-viewer';
+import { Slide } from '../components/slide';
+
+export default {
+  title: 'Components/SlideViewer',
+  component: SlideViewer
+};
+
+export const Primary = ({ scale }) => (
+  <SlideViewer scale={scale}>
+    <Slide>
+      <Heading>Hello World, Spectacle ✨</Heading>
+    </Slide>
+  </SlideViewer>
+);
+
+Primary.args = { scale: 0.2 };
+Primary.argTypes = {
+  scale: { control: { type: 'number', min: 0.1, max: 1, step: 0.1 } }
+};
+Primary.propTypes = {
+  scale: PropTypes.number.isRequired
+};

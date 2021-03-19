c855604c0e39a8b23ac6bbb92c9a767491922207
Grant Sander <gksander93@gmail.com>
2021-03-19 09:26:35 -0500

    Update lint command

--- editor/src/hooks/use-preview-window.js
+++ editor/src/hooks/use-preview-window.js
@@ -1,16 +1,10 @@
 import { useCallback } from 'react';
 import { navigate } from '@reach/router';
-import { useSelector } from 'react-redux';
-import { activeSlideSelector, slidesSelector } from '../slices/deck-slice';
 
 export const usePreviewWindow = () => {
-  const slideJson = useSelector(slidesSelector);
-  const activeSlideJson = useSelector(activeSlideSelector);
-
   const handleOpenPreviewWindow = useCallback(async () => {
-    const index = slideJson.indexOf(activeSlideJson);
     await navigate(`/preview-deck?slideIndex=${0}&stepIndex=0`);
-  }, [activeSlideJson, slideJson]);
+  }, []);
 
   return { handleOpenPreviewWindow };
 };

f3c2d31213f77c8cf0db159a489091d4cb2c72f9
Carlos Kelly <me@carlos.dev>
2021-03-19 12:05:02 -0500

    Add ability to edit colors from the theme.

--- editor/src/components/inspector/document-inspector.js
+++ editor/src/components/inspector/document-inspector.js
@@ -1,51 +1,11 @@
-import React, { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React from 'react';
 import { Pane } from './inspector-styles';
-import { deckSlice, themeSelector } from '../../slices/deck-slice';
-import { ColorPickerInput } from '../inputs/color';
+import { ThemeValues } from './theme-values';
 
 export const DocumentInspector = () => {
-  const dispatch = useDispatch();
-  const theme = useSelector(themeSelector);
-  const [primaryColor, setPrimaryColor] = useState('');
-  const [secondaryColor, setSecondaryColor] = useState('');
-  const [tertiaryColor, setTertiaryColor] = useState('');
-
-  useEffect(() => {
-    setPrimaryColor(theme?.colors?.primary || '');
-    setSecondaryColor(theme?.colors?.secondary || '');
-    setTertiaryColor(theme?.colors?.tertiary || '');
-  }, [theme]);
-
   return (
     <Pane>
-      <ColorPickerInput
-        onChangeInput={setPrimaryColor}
-        label="Primary Theme Value"
-        onUpdateValue={(value) =>
-          dispatch(deckSlice.actions.updateThemeColors({ primary: value }))
-        }
-        validValue={theme?.colors?.primary}
-        value={primaryColor}
-      />
-      <ColorPickerInput
-        onChangeInput={setSecondaryColor}
-        label="Secondary Theme Value"
-        onUpdateValue={(value) =>
-          dispatch(deckSlice.actions.updateThemeColors({ secondary: value }))
-        }
-        validValue={theme?.colors?.secondary}
-        value={secondaryColor}
-      />
-      <ColorPickerInput
-        onChangeInput={setTertiaryColor}
-        label="Tertiary Theme Value"
-        onUpdateValue={(value) =>
-          dispatch(deckSlice.actions.updateThemeColors({ tertiary: value }))
-        }
-        validValue={theme?.colors?.tertiary}
-        value={tertiaryColor}
-      />
+      <ThemeValues />
     </Pane>
   );
 };
---
+++ editor/src/components/inspector/theme-values.tsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { useDispatch, useSelector } from 'react-redux';
+import { deckSlice, themeSelector } from '../../slices/deck-slice';
+import { ColorPickerInput } from '../inputs/color';
+import { SpectacleTheme } from '../../types/theme';
+
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: 50% 50%;
+  grid-column-gap: 10px;
+  width: calc(100% - 10px);
+`;
+
+export const ThemeValues = () => {
+  const dispatch = useDispatch();
+  const themeValues = useSelector<{}, SpectacleTheme>(themeSelector);
+  const [inputState, setInputState] = useState(themeValues);
+
+  return (
+    <Container>
+      {'colors' in themeValues &&
+        Object.keys(themeValues.colors).map((colorKey) => (
+          <ColorPickerInput
+            onChangeInput={(value) =>
+              setInputState((prevState) => {
+                return {
+                  ...prevState,
+                  colors: { ...prevState.colors, [colorKey]: value }
+                };
+              })
+            }
+            key={`${colorKey}-color-value`}
+            label={
+              colorKey[0].toUpperCase() + colorKey.slice(1, colorKey.length)
+            }
+            onUpdateValue={(value) =>
+              dispatch(
+                deckSlice.actions.updateThemeColors({ [colorKey]: value })
+              )
+            }
+            validValue={themeValues?.colors[colorKey]}
+            value={inputState.colors[colorKey]}
+          />
+        ))}
+    </Container>
+  );
+};

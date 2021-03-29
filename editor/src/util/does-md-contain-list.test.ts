100ae94f2964d198cf864da97067babf35293c2e
Grant Sander <gksander93@gmail.com>
2021-03-29 10:07:13 -0500

    Util for checking if MD string contains list

---
+++ editor/src/util/does-md-contain-list.test.ts
@@ -0,0 +1,55 @@
+import { doesMdContainList } from './does-md-contain-list';
+
+const pairs = [
+  [`- I'm a list?`, true],
+  [`-I'm a list?`, false],
+  [`# Is this tricky title - a list?`, false],
+  [
+    `
+    # With other content
+    
+    - Lists still work
+  `,
+    true
+  ],
+  [`* I'm a list?`, true],
+  [`*I'm a list?`, false],
+  [`# Is this tricky title * a list?`, false],
+  [
+    `
+    # With other content
+    
+    * Lists still work
+  `,
+    true
+  ],
+  [`+ I'm a list?`, true],
+  [`+I'm a list?`, false],
+  [`# Is this tricky title + a list?`, false],
+  [
+    `
+    # With other content
+    
+    + Lists still work
+  `,
+    true
+  ],
+  [`1. I'm a list?`, true],
+  [`1 I'm a list?`, false],
+  [`2. I'm a list?`, true],
+  [`# Is this tricky title 1. a list?`, false],
+  [
+    `
+    # With other content
+    
+    1. Lists still work
+    `,
+    true
+  ]
+];
+
+describe('doesMdContainList', () => {
+  it.each(pairs)('doesMdContainList(%p)', (content, expected) => {
+    expect(doesMdContainList(String(content))).toBe(expected);
+  });
+});

01b26aa56d689f44bca5f7caaba08e3f5ad62dd6
Carlos Kelly <me@carlos.dev>
2021-04-05 13:15:54 -0500

    Update Spectacle Version

--- editor/src/templates/basic-layouts.ts
+++ editor/src/templates/basic-layouts.ts
@@ -1,10 +1,15 @@
 import { v4 } from 'uuid';
+import { indentNormalizer } from 'spectacle';
 import { DeckElement } from '../types/deck-elements';
 
 export const basicLayout = (): DeckElement[] => [
   {
     component: 'Markdown',
     id: v4(),
-    children: '# Second Slide'
+    children: indentNormalizer(`
+      # Second Slide
+
+      This is a paragraph.
+    `)
   }
 ];

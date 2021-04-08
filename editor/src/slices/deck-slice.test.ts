34752e4e77e0126a9fccf8bbd2af848086bab3bd
Carlos Kelly <me@carlos.dev>
2021-04-08 10:03:07 -0500

    Use Entity Adapter for Deck Slice

---
+++ editor/src/slices/deck-slice.test.ts
@@ -0,0 +1,21 @@
+// import { defaultTheme } from 'spectacle';
+// import { basicLayout } from '../templates/basic-layouts';
+// import { DeckSlide } from '../types/deck-elements';
+// import { deckSlice } from './deck-slice';
+
+// describe('deck-slice', () => {
+//   test('boo', () => {
+//     const slide: DeckSlide = { children: [], id: '123', component: 'Slide' };
+//     const nextState = deckSlice.reducer(
+//       {
+//         slides: [slide],
+//         activeSlide: slide,
+//         editableElementId: null,
+//         theme: defaultTheme
+//       },
+//       deckSlice.actions.applyLayoutToSlide(basicLayout())
+//     );
+
+//     console.log(nextState);
+//   });
+// });

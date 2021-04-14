621932626fbb3b727b97a3d5369c8983f9ada0c9
Brian Mathews <brian.mathews@formidable.com>
2021-04-14 08:40:27 -0700

    Store theme size as number

--- src/components/inspector/theme-values.tsx
+++ src/components/inspector/theme-values.tsx
@@ -53,7 +53,7 @@ export const ThemeValues = () => {
                 }
                 dispatch(
                   deckSlice.actions.updateThemeSize({
-                    [sizeKey]: value
+                    [sizeKey]: parseInt(value)
                   })
                 );
               }}

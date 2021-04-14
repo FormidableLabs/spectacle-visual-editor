8be51e981f635fca7ccbb4ac36a0a7589df3177e
Carlos Kelly <carlos.paelinck@formidable.com>
2021-04-14 10:44:34 -0500

    Visually show layout templates (#47)
    * Show Slides for Layout Templates
    
    * Updated Pane for Layout Inspector
    
    * Ensure Slide Wrapper has correct spacing and alignment.

--- src/components/inspector/layout-inspector.tsx
+++ src/components/inspector/layout-inspector.tsx
@@ -1,20 +1,54 @@
 import React from 'react';
-import { Button, Pane } from 'evergreen-ui';
+import styled from 'styled-components';
 import { useDispatch } from 'react-redux';
 import { deckSlice } from '../../slices/deck-slice';
-import { basicLayout } from '../../templates/basic-layouts';
+import { Pane } from './inspector-styles';
+import { basicLayout, codePaneLayout } from '../../templates/basic-layouts';
+import { Slide } from '../slide/slide';
+import { SlideViewerWrapper } from '../slide/slide-viewer/slide-viewer-wrapper';
+import { generateInternalSlideTree } from '../slide/slide-generator';
+import { DeckElement, DeckSlide } from '../../types/deck-elements';
+
+const layouts: { [key: string]: () => DeckElement[] } = {
+  basicLayout,
+  codePaneLayout
+};
+
+const SlideWrapper = styled.div`
+  overflow: hidden;
+  margin: 10px 0;
+  display: flex;
+  justify-content: center;
+`;
 
 export const LayoutInspector = () => {
   const dispatch = useDispatch();
   return (
     <Pane>
-      <Button
-        onClick={() =>
-          dispatch(deckSlice.actions.applyLayoutToSlide(basicLayout()))
-        }
-      >
-        Apply Basic Layout
-      </Button>
+      <SlideViewerWrapper>
+        {Object.keys(layouts).map((layoutKey) => (
+          <SlideWrapper key={`${layoutKey}-key`}>
+            <Slide
+              id={`${layoutKey}-template`}
+              scale={0.18}
+              slideProps={{
+                containerStyle: {},
+                onSlideClick() {
+                  dispatch(
+                    deckSlice.actions.applyLayoutToSlide(layouts[layoutKey]())
+                  );
+                }
+              }}
+            >
+              {(layouts[layoutKey]() as DeckSlide[]).map(
+                generateInternalSlideTree as (
+                  opt: DeckSlide
+                ) => React.ReactElement
+              )}
+            </Slide>
+          </SlideWrapper>
+        ))}
+      </SlideViewerWrapper>
     </Pane>
   );
 };

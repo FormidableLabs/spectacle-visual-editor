7ff1856e10b33375db0d5effc4d1a100a22484a6
Carlos Kelly <me@carlos.dev>
2021-04-14 07:37:09 -0500

    Updated Pane for Layout Inspector

--- src/components/inspector/layout-inspector.tsx
+++ src/components/inspector/layout-inspector.tsx
@@ -1,19 +1,13 @@
 import React from 'react';
-import { Pane } from 'evergreen-ui';
+import styled from 'styled-components';
 import { useDispatch } from 'react-redux';
 import { deckSlice } from '../../slices/deck-slice';
+import { Pane } from './inspector-styles';
 import { basicLayout, codePaneLayout } from '../../templates/basic-layouts';
 import { Slide } from '../slide/slide';
 import { SlideViewerWrapper } from '../slide/slide-viewer/slide-viewer-wrapper';
 import { generateInternalSlideTree } from '../slide/slide-generator';
 import { DeckElement, DeckSlide } from '../../types/deck-elements';
-import styled from 'styled-components';
-
-const Container = styled.div`
-  overflow-y: scroll;
-  overflow-x: hidden;
-  height: 100%;
-`;
 
 const layouts: { [key: string]: () => DeckElement[] } = {
   basicLayout,
@@ -24,31 +18,29 @@ export const LayoutInspector = () => {
   const dispatch = useDispatch();
   return (
     <Pane>
-      <Container>
-        <SlideViewerWrapper>
-          {Object.keys(layouts).map((layoutKey) => (
-            <Slide
-              key={`${layoutKey}-key`}
-              id={`${layoutKey}-template`}
-              scale={0.2}
-              slideProps={{
-                containerStyle: {},
-                onSlideClick() {
-                  dispatch(
-                    deckSlice.actions.applyLayoutToSlide(layouts[layoutKey]())
-                  );
-                }
-              }}
-            >
-              {(layouts[layoutKey]() as DeckSlide[]).map(
-                generateInternalSlideTree as (
-                  opt: DeckSlide
-                ) => React.ReactElement
-              )}
-            </Slide>
-          ))}
-        </SlideViewerWrapper>
-      </Container>
+      <SlideViewerWrapper>
+        {Object.keys(layouts).map((layoutKey) => (
+          <Slide
+            key={`${layoutKey}-key`}
+            id={`${layoutKey}-template`}
+            scale={0.2}
+            slideProps={{
+              containerStyle: {},
+              onSlideClick() {
+                dispatch(
+                  deckSlice.actions.applyLayoutToSlide(layouts[layoutKey]())
+                );
+              }
+            }}
+          >
+            {(layouts[layoutKey]() as DeckSlide[]).map(
+              generateInternalSlideTree as (
+                opt: DeckSlide
+              ) => React.ReactElement
+            )}
+          </Slide>
+        ))}
+      </SlideViewerWrapper>
     </Pane>
   );
 };

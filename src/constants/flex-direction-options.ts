6ae1e417ef302feb34058eeaecb5c403b25412c5
Lisa Munin <munin.lisa@gmail.com>
2021-04-28 06:31:09 -0700

    Add flex-direction selector to format inspector (#57)
    * Added flex-direction-options.ts to constants for consistancy in column vs row. Added flex-direction-controls.tsx selector for layout content.
    
    * Removed console.log
    
    * Changed the FLEX_DIRECTION_OPTIONS constant to be the values of FLEX_DIRECTION. Removed the need for additional state since the changed element through redux will do the proper ui rerenders
    
    * Update sample data to have flex dir.
    
    Co-authored-by: Carlos Kelly <me@carlos.dev>

---
+++ src/constants/flex-direction-options.ts
@@ -0,0 +1,8 @@
+// ENUM for specific comparisons
+export enum FLEX_DIRECTION {
+  column = 'column',
+  row = 'row'
+}
+
+// Options for mappings
+export const FLEX_DIRECTION_OPTIONS: string[] = Object.values(FLEX_DIRECTION);

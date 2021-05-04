9ff11728eb8f9235ab5b1beabe3476eac9a460ef
Sasha Schwartz <soft_circles@Sashas-MacBook-Pro.local>
2021-05-04 15:43:45 -0500

    Add format customization for list

--- src/components/inspector/list-controls.tsx
+++ src/components/inspector/list-controls.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { ChangeEvent } from 'react';
 import { ConstructedDeckElement } from '../../types/deck-elements';
-import { FormField, Switch } from 'evergreen-ui';
+import { FormField, Switch, TextInputField } from 'evergreen-ui';
 import { SelectInput } from '../inputs/select';
-import { cloneDeep, set } from 'lodash-es';
 import styled from 'styled-components';
-import { LIST_STYLE_TYPE_OPTIONS } from '../../constants/list-style-type-options';
+import {
+  LIST_STYLE_TYPE_OPTIONS,
+  LIST_COMPONENT_PROPS,
+  LIST_FONT_WEIGHT_OPTIONS,
+  LIST_TEXT_ALIGN_OPTIONS
+} from '../../constants/list-style-options';
+import { ColorPickerInput } from '../inputs/color';
+import { useRootSelector } from '../../store';
+import { themeSelector } from '../../slices/deck-slice';
 
 interface Props {
   selectedElement: ConstructedDeckElement | null;
@@ -17,10 +24,23 @@ export const ListControls: React.FC<Props> = ({
   selectedElement,
   editableElementChanged
 }) => {
+  const themeValues = useRootSelector(themeSelector);
   const shouldAnimateListItems =
     selectedElement?.props?.animateListItems || false;
   const listStyleType =
-    selectedElement?.props?.componentProps?.listStyleType || 'disc';
+    selectedElement?.props?.componentProps?.listStyleType ||
+    LIST_STYLE_TYPE_OPTIONS.DISC;
+  const color =
+    selectedElement?.props?.componentProps?.color || themeValues.colors.primary;
+  const fontSize =
+    selectedElement?.props?.componentProps?.fontSize ||
+    themeValues.fontSizes.text;
+  const fontWeight =
+    selectedElement?.props?.componentProps?.fontWeight ||
+    LIST_FONT_WEIGHT_OPTIONS.FOUR_HUNDRED;
+  const textAlign =
+    selectedElement?.props?.componentProps?.textAlign ||
+    LIST_TEXT_ALIGN_OPTIONS.LEFT;
 
   const onToggleAnimatedListItems = () => {
     editableElementChanged({
@@ -28,34 +48,79 @@ export const ListControls: React.FC<Props> = ({
     });
   };
 
-  const onListStyleTypeChanged = (val: string) => {
+  const onChangeComponentProps = (propName: string, val: string) => {
     if (selectedElement) {
-      const newEl = set(
-        cloneDeep(selectedElement),
-        'componentProps.listStyleType',
-        val
-      );
-      editableElementChanged(newEl);
+      editableElementChanged({
+        componentProps: {
+          ...selectedElement.props?.componentProps,
+          [propName]: val
+        }
+      });
     }
   };
 
   return (
     <Container>
+      <ColorPickerInput
+        label="Color"
+        onUpdateValue={(value) =>
+          onChangeComponentProps(LIST_COMPONENT_PROPS.COLOR, value)
+        }
+        validValue={color}
+        onChangeInput={(value) =>
+          onChangeComponentProps(LIST_COMPONENT_PROPS.COLOR, value)
+        }
+        value={color}
+      />
+      <TextInputField
+        label="Font Size"
+        value={fontSize}
+        onChange={(e: ChangeEvent<HTMLInputElement>) => {
+          const { value } = e.target;
+          if (/^\d+px$/g.test(value)) {
+            onChangeComponentProps(LIST_COMPONENT_PROPS.FONT_SIZE, value);
+          }
+        }}
+      />
+      <SelectInput
+        label="Font Weight"
+        value={fontWeight}
+        options={Object.values(LIST_FONT_WEIGHT_OPTIONS).map((op) => ({
+          value: op,
+          title: op
+        }))}
+        onValueChange={(value) =>
+          onChangeComponentProps(LIST_COMPONENT_PROPS.FONT_WEIGHT, value)
+        }
+      />
+      <SelectInput
+        label="Text Align"
+        value={textAlign}
+        options={Object.values(LIST_TEXT_ALIGN_OPTIONS).map((op) => ({
+          value: op,
+          title: op
+        }))}
+        onValueChange={(value) =>
+          onChangeComponentProps(LIST_COMPONENT_PROPS.TEXT_ALIGN, value)
+        }
+      />
+      <SelectInput
+        label="List Style Type"
+        value={listStyleType}
+        onValueChange={(value) =>
+          onChangeComponentProps(LIST_COMPONENT_PROPS.LIST_STYLE_TYPE, value)
+        }
+        options={Object.values(LIST_STYLE_TYPE_OPTIONS).map((op) => ({
+          value: op,
+          title: op
+        }))}
+      />
       <FormField label="Animate list items?">
         <Switch
           checked={shouldAnimateListItems}
           onChange={onToggleAnimatedListItems}
         />
       </FormField>
-      <SelectInput
-        label="List Style Type"
-        value={listStyleType}
-        onValueChange={onListStyleTypeChanged}
-        options={LIST_STYLE_TYPE_OPTIONS.map((op) => ({
-          value: op,
-          title: op
-        }))}
-      />
     </Container>
   );
 };
@@ -63,4 +128,5 @@ export const ListControls: React.FC<Props> = ({
 const Container = styled.div`
   display: grid;
   grid-gap: 10px;
+  margin-top: 10px;
 `;

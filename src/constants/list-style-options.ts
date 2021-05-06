4e55350d29874347f356b2f0bb04d86bf4faca62
Sasha Schwartz <sasha.schwartz@formidable.com>
2021-05-06 16:35:43 -0500

    Add format customization for list (#68)
    * Add format customization for list
    
    Co-authored-by: Sasha Schwartz <soft_circles@Sashas-MacBook-Pro.local>

---
+++ src/constants/list-style-options.ts
@@ -0,0 +1,31 @@
+export enum LIST_STYLE_TYPE_OPTIONS {
+  NONE = 'none',
+  DISC = 'disc',
+  CIRCLE = 'circle',
+  SQUARE = 'square',
+  LOWER_LATIN = 'lower-latin',
+  UPPER_LATIN = 'upper-latin',
+  LOWER_ROMAN = 'lower-roman',
+  UPPER_ROMAN = 'upper-roman',
+  LOWER_GREEK = 'lower-greek',
+  DECIMAL = 'decimal'
+}
+
+export enum LIST_FONT_WEIGHT_OPTIONS {
+  FOUR_HUNDRED = '400',
+  SIX_HUNDRED = '600'
+}
+
+export enum LIST_TEXT_ALIGN_OPTIONS {
+  LEFT = 'left',
+  CENTER = 'center',
+  RIGHT = 'right'
+}
+
+export enum LIST_COMPONENT_PROPS {
+  LIST_STYLE_TYPE = 'listStyleType',
+  COLOR = 'color',
+  FONT_SIZE = 'fontSize',
+  FONT_WEIGHT = 'fontWeight',
+  TEXT_ALIGN = 'textAlign'
+}

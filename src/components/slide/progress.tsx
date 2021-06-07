d04760ca7aa77dc2f9fc0dc22fddfad0d64f19aa
Kenan Yusuf <kenan.m.yusuf@gmail.com>
2021-06-07 16:06:45 +0100

    Foward ref to progerss for movable

--- src/components/slide/progress.tsx
+++ src/components/slide/progress.tsx
@@ -1,39 +1,34 @@
-import React, { FC } from 'react';
+import React, { forwardRef } from 'react';
 import { Progress } from 'spectacle';
 import styled from 'styled-components';
 import { position } from 'styled-system';
 import { useLocation } from '@reach/router';
 import { PATHS } from '../../constants/paths';
 
-const InternalProgress: FC<{ size: number; color: string }> = ({
-  size,
-  color,
-  ...props
-}) => {
+const InternalProgress = forwardRef<
+  HTMLDivElement,
+  { size: number; color: string }
+>(function InternalProgressWithRef({ size, color, ...props }, ref) {
   const location = useLocation();
   const isPreview = location.pathname === PATHS.PREVIEW_DECK;
 
   return (
-    <Container isPreview={isPreview} {...props}>
+    <Container isPreview={isPreview} ref={ref} {...props}>
       <Progress size={size} color={color} />
     </Container>
   );
-};
+});
 
 const Container = styled.div<{ isPreview: boolean }>`
   ${position}
+
+  ${({ isPreview }) =>
+    !isPreview &&
+    `
+      * {
+        pointer-events: none;
+      }
+  `}
 `;
-
-// const Container = styled.div<{ isPreview: boolean }>`
-//   ${position}
-
-//   ${({ isPreview }) =>
-//     !isPreview &&
-//     `
-//       * {
-//         pointer-events: none;
-//       }
-//   `}
-// `;
 
 export default InternalProgress;

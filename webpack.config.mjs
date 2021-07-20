4dfeb0411f73c36f3dd912ee46d7d23243881246
AJ Livingston <livingston.aj@gmail.com>
2021-07-20 18:06:58 -0400

    build: enable source maps in development mode

--- webpack.config.mjs
+++ webpack.config.mjs
@@ -4,43 +4,48 @@ import webpack from 'webpack';
 
 const __dirname = path.resolve();
 
-export default {
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'app.js'
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-    modules: [path.join(__dirname, 'src'), 'node_modules']
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/
-      },
-      {
-        test: /\.js/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
+export default (env, argv) => {
+  const isDevelopment = argv.mode === 'development';
+
+  return {
+    devtool: isDevelopment ? 'cheap-module-source-map' : false,
+    output: {
+      path: path.resolve(__dirname, 'build'),
+      filename: 'app.js'
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js'],
+      modules: [path.join(__dirname, 'src'), 'node_modules']
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/
         },
-        resolve: {
-          fullySpecified: false
+        {
+          test: /\.js/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader'
+          },
+          resolve: {
+            fullySpecified: false
+          }
         }
-      }
+      ]
+    },
+    devServer: {
+      historyApiFallback: true
+    },
+    plugins: [
+      new webpack.ProvidePlugin({
+        process: 'process/browser'
+      }),
+      new HtmlWebpackPlugin({
+        template: './src/index.html'
+      })
     ]
-  },
-  devServer: {
-    historyApiFallback: true
-  },
-  plugins: [
-    new webpack.ProvidePlugin({
-      process: 'process/browser'
-    }),
-    new HtmlWebpackPlugin({
-      template: './src/index.html'
-    })
-  ]
+  };
 };

99d1f026e78c5d29e1c5c52df8290075e5333b6f
Melvin Chen <mchen1626@gmail.com>
2022-03-14 17:36:01 -0400

    move crop capability to image controls, fix selection frame on images

--- src/components/inspector/image-controls.tsx
+++ src/components/inspector/image-controls.tsx
@@ -1,9 +1,47 @@
 import React, { ChangeEvent } from 'react';
 import { ElementControlsProps } from './element-controls-props';
-import { TextInputField } from 'evergreen-ui';
+import {
+  Button,
+  CrossIcon,
+  IconButton,
+  Popover,
+  TextInputField,
+  TickIcon,
+  toaster
+} from 'evergreen-ui';
 import { isValidUrl } from '../../util/is-valid-url';
 import { FreeMovementControls } from './free-movement-controls';
 import { ResizeControls } from './resize-controls';
+import styled from 'styled-components';
+import Cropper from 'cropperjs';
+
+const CropperImageContainer = styled.div`
+  max-width: 400px;
+  height: 100%;
+`;
+
+const CropperImage = styled.img`
+  display: block;
+  max-width: 100%;
+`;
+
+const CropperButtonContainer = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  padding: 8px;
+  box-sizing: border-box;
+
+  button:first-child {
+    margin-right: 4px;
+  }
+`;
+
+const CropButton = styled(Button)`
+  width: 100%;
+  justify-content: center;
+`;
 
 export const ImageControls: React.FC<ElementControlsProps> = ({
   selectedElement,
@@ -13,6 +51,19 @@ export const ImageControls: React.FC<ElementControlsProps> = ({
   const [inputState, setInputState] = React.useState({
     desiredSrc
   });
+  const [isCropModalOpen, setCropModalOpen] = React.useState<boolean>(false);
+  const [
+    dialogImageRef,
+    setDialogImageRef
+  ] = React.useState<HTMLImageElement | null>();
+  const cropperInstance = React.useRef<Cropper | undefined>();
+  React.useEffect(() => {
+    if (dialogImageRef && isCropModalOpen) {
+      cropperInstance.current = new Cropper(dialogImageRef, {
+        // aspectRatio: 16 / 9
+      });
+    }
+  }, [dialogImageRef, isCropModalOpen]);
 
   const handleValueChanged = React.useCallback(
     (propName: string, value) => {
@@ -48,6 +99,44 @@ export const ImageControls: React.FC<ElementControlsProps> = ({
           }
         }}
       />
+      <Popover
+        onOpen={() => setCropModalOpen(true)}
+        onClose={() => {
+          setCropModalOpen(false);
+        }}
+        content={({ close }) => (
+          <CropperImageContainer>
+            <CropperImage
+              src={selectedElement?.props?.src}
+              ref={(r) => setDialogImageRef(r)}
+            />
+            <CropperButtonContainer>
+              <IconButton
+                onClick={() => {
+                  close();
+                  const e = cropperInstance.current?.getCroppedCanvas();
+
+                  if (!e) {
+                    return toaster.warning('Could not crop the image.');
+                  }
+                  handleValueChanged('croppedSrc', e.toDataURL());
+                }}
+                icon={TickIcon}
+                height={48}
+              />
+              <IconButton
+                onClick={() => {
+                  close();
+                }}
+                icon={CrossIcon}
+                height={48}
+              />
+            </CropperButtonContainer>
+          </CropperImageContainer>
+        )}
+      >
+        <CropButton>Crop Image</CropButton>
+      </Popover>
     </div>
   );
 };

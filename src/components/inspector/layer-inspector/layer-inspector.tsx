1d60c356b97c4b5b0e86d48c4fc7683b096fd5cb
Scott Nguyen <scottqmn@gmail.com>
2022-05-24 18:06:22 -0700

    Cleanup old logic

--- src/components/inspector/layer-inspector/layer-inspector.tsx
+++ src/components/inspector/layer-inspector/layer-inspector.tsx
@@ -1,13 +1,5 @@
 import React, { FC, useState, useMemo, useEffect, useCallback } from 'react';
-import { useRootSelector } from '../../../store';
-import { cloneDeep } from 'lodash-es';
-import { ConstructedDeckElement } from '../../../types/deck-elements';
-import {
-  activeSlideSelector,
-  selectedElementSelector,
-  deckSlice,
-  hoveredEditableElementIdSelector
-} from '../../../slices/deck-slice';
+import { useDispatch, useSelector } from 'react-redux';
 import { DndProvider } from 'react-dnd';
 import {
   Tree,
@@ -15,19 +7,20 @@ import {
   MultiBackend,
   PlaceholderRender
 } from '@minoru/react-dnd-treeview';
-import { HTML5Backend } from 'react-dnd-html5-backend';
-import { useDispatch, useSelector } from 'react-redux';
-import styled from 'styled-components';
-import { isDeckElement } from '../../../util/is-deck-element';
-import { LayerDragWrapper, Layer } from '../../helpers/layer-drag-wrapper';
-import {
-  ElementCard,
-  ElementCardTree,
-  ElementCardDisplay
-} from './layers-element-card';
-import { moveArrayItem } from '../../../util/move-array-item';
 import { defaultTheme } from 'evergreen-ui';
-import { CONTAINER_ELEMENTS } from '../../../types/deck-elements';
+import styled from 'styled-components';
+import { ElementCard, ElementCardDisplay } from './layers-element-card';
+import { useRootSelector } from '../../../store';
+import {
+  activeSlideSelector,
+  selectedElementSelector,
+  deckSlice,
+  hoveredEditableElementIdSelector
+} from '../../../slices/deck-slice';
+import {
+  CONTAINER_ELEMENTS,
+  ConstructedDeckElement
+} from '../../../types/deck-elements';
 
 type TreeNode = {
   id: any;
@@ -153,201 +146,10 @@ const deconstructSlide = (slide: ConstructedDeckElement) => {
 };
 
 export const LayerInspector: FC = () => {
+  const dispatch = useDispatch();
   const activeSlide = useRootSelector(activeSlideSelector);
-  const activeSlideElements = useMemo(
-    () => (activeSlide?.children || []).filter(isDeckElement),
-    [activeSlide]
-  );
-  const [localElements, setLocalElements] = useState(activeSlideElements);
   const selectedElement = useSelector(selectedElementSelector);
   const hoveredElementId = useSelector(hoveredEditableElementIdSelector);
-  const [collapsedLayers, setCollapsedLayers] = useState<Array<String>>([]);
-
-  const dispatch = useDispatch();
-
-  // Keep local children in sync with slide children
-  useEffect(() => {
-    setLocalElements(activeSlideElements);
-  }, [activeSlideElements]);
-
-  // Move a local item as its dragged
-  const moveElement = useCallback(
-    (currentLocation: Layer, nextLocation: Layer) => {
-      setLocalElements((localElements) => {
-        // Only allow movement within the same parent context in this function
-        if (currentLocation.parentId !== nextLocation.parentId) {
-          return localElements;
-        }
-
-        // If parentId is defined, then the element is nested
-        // If it is not defined, the element is a top-level element
-        if (currentLocation.parentId) {
-          const clonedLocalElements = cloneDeep(localElements);
-          const parentIndex = clonedLocalElements.findIndex(
-            (el) => el.id === (currentLocation.parentId as string)
-          );
-          const parent = clonedLocalElements[parentIndex];
-
-          if (!parent || !Array.isArray(parent.children)) {
-            return clonedLocalElements;
-          }
-
-          const parentChildren = parent?.children as ConstructedDeckElement[];
-          const currentIndex = parentChildren.findIndex((el) => {
-            return el.id === currentLocation.id;
-          });
-          const nextIndex = parentChildren.findIndex((el) => {
-            return el.id === nextLocation.id;
-          });
-
-          const reorderedElementChildren = moveArrayItem(
-            parentChildren,
-            currentIndex,
-            nextIndex
-          );
-
-          clonedLocalElements[parentIndex].children = reorderedElementChildren;
-          return clonedLocalElements;
-        }
-
-        const currentIndex = localElements.findIndex(
-          (el) => el.id === currentLocation.id
-        );
-        const nextIndex = localElements.findIndex(
-          (el) => el.id === nextLocation.id
-        );
-        return moveArrayItem(localElements, currentIndex, nextIndex);
-      });
-    },
-    []
-  );
-
-  const moveElementInside = useCallback(
-    (
-      currentLocation: Layer,
-      nextLocation: Layer,
-      direction: 'top' | 'bottom'
-    ) => {
-      setLocalElements((localElements) => {
-        const clonedLocalElements = cloneDeep(localElements);
-        const parentIndex = clonedLocalElements.findIndex(
-          (el) => el.id === nextLocation.id
-        );
-        const parent = clonedLocalElements[parentIndex];
-        const parentChildren = parent.children;
-        const itemInQuestion = clonedLocalElements.find(
-          (el) => el.id === currentLocation.id
-        );
-
-        if (
-          itemInQuestion === undefined ||
-          parentChildren === undefined ||
-          !Array.isArray(parentChildren)
-        ) {
-          return localElements;
-        }
-
-        if (direction === 'top') {
-          (parentChildren as ConstructedDeckElement[]).unshift(itemInQuestion);
-        } else if (direction === 'bottom') {
-          parentChildren.push(itemInQuestion);
-        }
-
-        const x = clonedLocalElements.filter(
-          (el) => el.id !== currentLocation.id
-        );
-        return x;
-      });
-    },
-    []
-  );
-
-  const moveElementOutside = useCallback(
-    (currentLocation: Layer, nextLocation: Layer) => {
-      setLocalElements((localElements) => {
-        if (currentLocation.parentId === undefined) return localElements;
-        const clonedLocalElements = cloneDeep(localElements);
-
-        const currentParentIndex = clonedLocalElements.findIndex(
-          (el) => el.id === currentLocation.parentId
-        );
-        const nextLocationIndex = clonedLocalElements.findIndex(
-          (el) => el.id === nextLocation.id
-        );
-        const currentParent = clonedLocalElements[currentParentIndex];
-        const itemInQuestion = (currentParent.children as ConstructedDeckElement[]).find(
-          (el) => el.id === currentLocation.id
-        );
-        if (!itemInQuestion) return localElements;
-        const updatedParent = (currentParent.children as ConstructedDeckElement[]).filter(
-          (el) => el.id !== currentLocation.id
-        );
-
-        clonedLocalElements[currentParentIndex].children = updatedParent;
-        clonedLocalElements.splice(nextLocationIndex, 0, itemInQuestion);
-
-        return clonedLocalElements;
-      });
-    },
-    []
-  );
-
-  // Update the order with the local order
-  const commitChangedOrder = useCallback(
-    (previousParent: string | undefined, nextParent: string | undefined) => {
-      // commitChangedOrder is called with a stale version of localElements.
-      // Use update function to get the most recent change. Return original value.
-      setLocalElements((localElements) => {
-        const nextLocationIndex = localElements.findIndex(
-          (el) => el.id === nextParent
-        );
-
-        // Update drop location
-        const elementsToUpdate =
-          typeof nextParent === 'string'
-            ? localElements[nextLocationIndex].children
-            : localElements;
-        if (!Array.isArray(elementsToUpdate)) {
-          return localElements;
-        }
-        dispatch(
-          deckSlice.actions.reorderActiveSlideElements({
-            elementIds: elementsToUpdate.map((element) => element.id),
-            parentId:
-              typeof nextParent === 'string'
-                ? localElements[nextLocationIndex].id
-                : undefined
-          })
-        );
-
-        // If element has moved outside of parent, update previous parent
-        if (previousParent !== nextParent) {
-          const previousLocationIndex = localElements.findIndex(
-            (el) => el.id === previousParent
-          );
-
-          const elementsToUpdate =
-            typeof previousParent === 'string'
-              ? localElements[previousLocationIndex].children
-              : localElements;
-          if (!Array.isArray(elementsToUpdate)) {
-            return localElements;
-          }
-          dispatch(
-            deckSlice.actions.reorderActiveSlideElements({
-              elementIds: elementsToUpdate.map((element) => element.id),
-              parentId:
-                typeof previousParent === 'string'
-                  ? localElements[previousLocationIndex].id
-                  : undefined
-            })
-          );
-        }
-        return localElements;
-      });
-    },
-    [dispatch]
-  );
 
   const hoverElement = useCallback(
     (id) => {
@@ -367,17 +169,6 @@ export const LayerInspector: FC = () => {
     [dispatch]
   );
 
-  const handleExpand = useCallback((id) => {
-    setCollapsedLayers((currentCollapsedLayers) => {
-      if (currentCollapsedLayers.includes(id)) {
-        return currentCollapsedLayers.filter((layerId) => layerId !== id);
-      } else {
-        return [...currentCollapsedLayers, id];
-      }
-    });
-  }, []);
-
-  // tree
   const treeRoot = useMemo(() => activeSlide?.id || '', [activeSlide]);
   const [treeData, setTreeData] = useState<TreeNode[]>([]);
 
@@ -441,7 +232,7 @@ export const LayerInspector: FC = () => {
     const isParentSelected = checkIfParentSelected(node, depth);
 
     return (
-      <ElementCardTree
+      <ElementCard
         element={element}
         isHovered={isHovered}
         isSelected={isSelected}
@@ -499,71 +290,6 @@ export const LayerInspector: FC = () => {
               dropTargetOffset={10}
             />
           </TreeWrap>
-        </DndProvider>
-        <DndProvider backend={HTML5Backend}>
-          {localElements.map((element, index) => {
-            const isHovered = element.id === hoveredElementId;
-            const isSelected = element.id === selectedElement?.id;
-            const isExpanded = !collapsedLayers.includes(element.id);
-            const isContainerElement = CONTAINER_ELEMENTS.includes(
-              element.component
-            );
-            const isChildSelected =
-              Array.isArray(element.children) &&
-              !!element.children.find(
-                (child) => child.id === selectedElement?.id
-              );
-
-            return (
-              <LayerDragWrapper
-                index={index}
-                key={element.id}
-                onDrag={moveElement}
-                onDrop={commitChangedOrder}
-                parentId={undefined}
-                id={element.id}
-                isContainerElement={isContainerElement}
-                onDragInside={moveElementInside}
-                onDragOutside={moveElementOutside}
-              >
-                <ElementCard
-                  element={element}
-                  isHovered={isHovered}
-                  isSelected={isSelected}
-                  isExpanded={isExpanded}
-                  isParentSelected={!isExpanded && isChildSelected}
-                  handleExpand={() => handleExpand(element.id)}
-                  onClick={() => selectElement(element.id)}
-                  onMouseEnter={() => hoverElement(element.id)}
-                  onMouseLeave={unhoverElement}
-                />
-                {isExpanded &&
-                  Array.isArray(element.children) &&
-                  element.children.map((childElement, childElementIndex) => (
-                    <LayerDragWrapper
-                      key={childElement.id}
-                      index={childElementIndex}
-                      parentId={element.id}
-                      id={childElement.id}
-                      onDrag={moveElement}
-                      onDrop={commitChangedOrder}
-                      isContainerElement={false}
-                    >
-                      <ElementCard
-                        element={childElement}
-                        isHovered={childElement.id === hoveredElementId}
-                        isSelected={childElement.id === selectedElement?.id}
-                        isParentSelected={isSelected}
-                        isChildElement
-                        onClick={() => selectElement(childElement.id)}
-                        onMouseEnter={() => hoverElement(childElement.id)}
-                        onMouseLeave={unhoverElement}
-                      />
-                    </LayerDragWrapper>
-                  ))}
-              </LayerDragWrapper>
-            );
-          })}
         </DndProvider>
       </Layers>
     </Container>
--- src/components/inspector/layer-inspector/layers-element-card.tsx
+++ src/components/inspector/layer-inspector/layers-element-card.tsx
@@ -29,136 +29,7 @@ const ELEMENT_ICONS = {
   Notes: AnnotationIcon
 };
 
-interface Props {
-  isHovered: boolean;
-  isSelected: boolean;
-  isParentSelected: boolean;
-  isChildElement?: boolean;
-  isExpanded?: boolean;
-  element: ConstructedDeckElement;
-  onClick: (e: MouseEvent<HTMLDivElement>) => void;
-  onMouseEnter: (e: MouseEvent<HTMLDivElement>) => void;
-  onMouseLeave: (e: MouseEvent<HTMLDivElement>) => void;
-  handleExpand?: () => void;
-}
-
-/**
- * Layers tab element card
- * @param element DeckElement
- */
-export const ElementCard: React.FC<Props> = ({
-  element,
-  isHovered,
-  isSelected,
-  isParentSelected,
-  isChildElement,
-  isExpanded,
-  onClick,
-  onMouseEnter,
-  onMouseLeave,
-  handleExpand
-}) => {
-  const hasChildren = Array.isArray(element.children);
-
-  const elementTitle = React.useMemo(() => {
-    if (isMdElement(element)) {
-      return String(element.children);
-    } else if (isImageElement(element)) {
-      return String(element?.props?.src);
-    } else {
-      return element.component;
-    }
-  }, [element]);
-
-  return (
-    <Layer
-      role="button"
-      title={element.component}
-      isHovered={isHovered}
-      isSelected={isSelected}
-      isParentSelected={isParentSelected}
-      isChildElement={isChildElement}
-      onClick={onClick}
-      onMouseEnter={onMouseEnter}
-      onMouseLeave={onMouseLeave}
-    >
-      <Icon
-        icon={ELEMENT_ICONS[element.component as keyof typeof ELEMENT_ICONS]}
-        size={14}
-      />
-      <Title>{elementTitle}</Title>
-
-      {hasChildren && (
-        <IconButton
-          icon={isExpanded ? ChevronDownIcon : ChevronLeftIcon}
-          height={24}
-          appearance="minimal"
-          marginLeft="auto"
-          onClick={(e: MouseEvent<HTMLButtonElement>) => {
-            e.stopPropagation();
-            if (handleExpand) {
-              handleExpand();
-            }
-          }}
-        />
-      )}
-    </Layer>
-  );
-};
-
-interface DisplayProps {
-  element: ConstructedDeckElement;
-  depth: number;
-  isSelected: boolean;
-  isParentSelected: boolean;
-  isExpanded?: boolean;
-}
-export const ElementCardDisplay: React.FC<DisplayProps> = ({
-  element,
-  depth,
-  isSelected,
-  isParentSelected,
-  isExpanded
-}) => {
-  const hasChildren = Array.isArray(element.children);
-
-  const elementTitle = React.useMemo(() => {
-    if (isMdElement(element)) {
-      return String(element.children);
-    } else if (isImageElement(element)) {
-      return String(element?.props?.src);
-    } else {
-      return element.component;
-    }
-  }, [element]);
-
-  return (
-    <LayerTree
-      title={element.component}
-      isSelected={isSelected}
-      isParentSelected={isParentSelected}
-      depth={depth}
-      style={{ opacity: 0.2 }}
-    >
-      <Icon
-        icon={ELEMENT_ICONS[element.component as keyof typeof ELEMENT_ICONS]}
-        size={14}
-      />
-      <Title>{elementTitle} display</Title>
-
-      {hasChildren && (
-        <IconButton
-          icon={isExpanded ? ChevronDownIcon : ChevronLeftIcon}
-          height={24}
-          appearance="minimal"
-          marginLeft="auto"
-        />
-      )}
-    </LayerTree>
-  );
-};
-
-interface TreeProps {
+type Props = {
   element: ConstructedDeckElement;
   depth: number;
   isHovered: boolean;
@@ -171,8 +42,9 @@ interface TreeProps {
   onMouseEnter: (e: MouseEvent<HTMLDivElement>) => void;
   onMouseLeave: (e: MouseEvent<HTMLDivElement>) => void;
   handleExpand?: () => void;
-}
-export const ElementCardTree: React.FC<TreeProps> = ({
+};
+
+export const ElementCard: React.FC<Props> = ({
   element,
   depth,
   isHovered,
@@ -199,7 +71,7 @@ export const ElementCardTree: React.FC<TreeProps> = ({
   }, [element]);
 
   return (
-    <LayerTree
+    <Layer
       role="button"
       title={element.component}
       isHovered={isHovered}
@@ -232,7 +104,62 @@ export const ElementCardTree: React.FC<TreeProps> = ({
           }}
         />
       )}
-    </LayerTree>
+    </Layer>
+  );
+};
+
+type DisplayProps = Omit<
+  Props,
+  | 'isHovered'
+  | 'isChildElement'
+  | 'isDragging'
+  | 'onClick'
+  | 'onMouseEnter'
+  | 'onMouseLeave'
+  | 'handleExpand'
+>;
+
+export const ElementCardDisplay: React.FC<DisplayProps> = ({
+  element,
+  depth,
+  isSelected,
+  isParentSelected,
+  isExpanded
+}) => {
+  const hasChildren = Array.isArray(element.children);
+
+  const elementTitle = React.useMemo(() => {
+    if (isMdElement(element)) {
+      return String(element.children);
+    } else if (isImageElement(element)) {
+      return String(element?.props?.src);
+    } else {
+      return element.component;
+    }
+  }, [element]);
+
+  return (
+    <Layer
+      isSelected={isSelected}
+      isParentSelected={isParentSelected}
+      depth={depth}
+      transparent
+    >
+      <Icon
+        icon={ELEMENT_ICONS[element.component as keyof typeof ELEMENT_ICONS]}
+        size={14}
+      />
+      <Title>{elementTitle}</Title>
+
+      {hasChildren && (
+        <IconButton
+          icon={isExpanded ? ChevronDownIcon : ChevronLeftIcon}
+          height={24}
+          appearance="minimal"
+          marginLeft="auto"
+        />
+      )}
+    </Layer>
   );
 };
 
@@ -241,37 +168,8 @@ const Layer = styled.div<{
   isHovered?: boolean;
   isParentSelected?: boolean;
   isChildElement?: boolean;
-}>`
-  cursor: grab;
-  display: flex;
-  align-items: center;
-  overflow: hidden;
-  height: 36px;
-  padding: 0 10px 0 ${(props) => (props.isChildElement ? '34px' : '10px')};
-  color: ${(props) =>
-    props.isSelected || props.isParentSelected
-      ? defaultTheme.colors.selected
-      : defaultTheme.colors.default};
-  background: ${(props) =>
-    props.isSelected
-      ? defaultTheme.colors.blue100
-      : props.isHovered
-      ? defaultTheme.colors.gray200
-      : props.isParentSelected
-      ? defaultTheme.colors.blue50
-      : 'none'};
-
-  &:focus {
-    border: 1px solid ${defaultTheme.colors.selected};
-  }
-`;
-
-const LayerTree = styled.div<{
-  isSelected?: boolean;
-  isHovered?: boolean;
-  isParentSelected?: boolean;
-  isChildElement?: boolean;
   isDragging?: boolean;
+  transparent?: boolean;
   depth: number;
 }>`
   cursor: grab;
@@ -294,6 +192,10 @@ const LayerTree = styled.div<{
       : props.isParentSelected
       ? defaultTheme.colors.blue50
       : 'none'};
+
+  opacity: ${(props) => (props.transparent ? 0.2 : 1)};
+  max-height: ${(props) => (props.isDragging ? '0px' : 'none')}
+  overflow: hidden;
 
   &:focus {
     border: 1px solid ${defaultTheme.colors.selected};

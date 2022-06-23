1a356d6e5b55177fb54c232b594e882a7da194d0
JDMathew <justindavidmathew@gmail.com>
2022-06-23 18:20:23 +0100

    narrow THEME_COLORS_BY_TYPE type

--- src/constants/color-swatches.ts
+++ src/constants/color-swatches.ts
@@ -8,14 +8,17 @@ export const SWATCH_NAMES = {
 // Sort Evergreen UI color palette into types
 const THEME_COLORS_BY_TYPE = Object.values(
   deprecatedDefaultTheme.palette
-).reduce<Record<string, string[]>>((colorTypes, color) => {
-  const newColorTypes = { ...colorTypes };
-  newColorTypes.dark = [...(colorTypes.dark || []), color.dark];
-  newColorTypes.base = [...(colorTypes.base || []), color.base];
-  newColorTypes.light = [...(colorTypes.light || []), color.light];
-  newColorTypes.lightest = [...(colorTypes.lightest || []), color.lightest];
-  return newColorTypes;
-}, {});
+).reduce<Record<'dark' | 'base' | 'light' | 'lightest', string[]>>(
+  (colorTypes, color) => {
+    const newColorTypes = { ...colorTypes };
+    newColorTypes.dark = [...colorTypes.dark, color.dark];
+    newColorTypes.base = [...colorTypes.base, color.base];
+    newColorTypes.light = [...colorTypes.light, color.light];
+    newColorTypes.lightest = [...colorTypes.lightest, color.lightest];
+    return newColorTypes;
+  },
+  { dark: [], base: [], light: [], lightest: [] }
+);
 
 // Flatten sorted object so that colors can be mapped over
 export const DEFAULT_COLORS = Object.values(THEME_COLORS_BY_TYPE).reduce(

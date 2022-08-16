fece416a73309729517e8268d21a258fc222e1c1
Carlos Kelly <me@carlos.dev>
2022-08-16 14:26:07 -0500

    Add Radix, PostCSS, and Tailwind. (#202)

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,9 @@ import { store } from './store';
 import { PATHS } from './constants/paths';
 import { theme } from './theme';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import * as Toast from '@radix-ui/react-toast';
+import './index.css';
+
 const container = document.getElementById('root');
 const root = createRoot(container!);
 
@@ -15,10 +18,13 @@ root.render(
   <Provider store={store}>
     <ThemeProvider value={mergeTheme(defaultTheme, theme)}>
       <BrowserRouter>
-        <Routes>
-          <Route path={PATHS.VISUAL_EDITOR} element={<VisualEditor />} />
-          <Route path={PATHS.PREVIEW_DECK} element={<PreviewDeck />} />
-        </Routes>
+        <Toast.Provider>
+          <Toast.Viewport className="absolute w-screen" />
+          <Routes>
+            <Route path={PATHS.VISUAL_EDITOR} element={<VisualEditor />} />
+            <Route path={PATHS.PREVIEW_DECK} element={<PreviewDeck />} />
+          </Routes>
+        </Toast.Provider>
       </BrowserRouter>
     </ThemeProvider>
   </Provider>
